d7e9de63af5f5f1cb880702ae7ae244a40404023
Julianna <juliannagoldbas@gmail.com>
2023-08-21 13:34:56 -0400

    adding lonely_integer.py

---
+++ lonely_integer.py
@@ -0,0 +1,14 @@
+"""
+HackerRank problem: Lonely Integer
+- Given an array of integers, where all elements except one element occur twice, find the unique element.
+EX. input: a = [1,2,3,4,3,2,1]
+    output: 4
+"""
+def lonelyinteger(a):
+    for i in a:
+        if a.count(i) == 1:
+            return i
+
+if __name__ == '__main__':
+    result = lonelyinteger([1,1,2,2,4])
+    print(result)
